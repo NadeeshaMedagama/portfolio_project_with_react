7488d412abd3d5823d557b589a26b4c0a0dbe600
Nadeesha Medagama <nadeeshamedagama@gmail.com>
2025-05-27 11:29:29 +0530

    header section updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export const Header = () => {
             <div className="container mx-auto px-6 py-4">
                 <div className="flex items-center justify-between">
                     <a href="#" className="text-2xl font-bold text-primary-light dark:text-primary-dark">
-                        YourName
+                        NM
                     </a>
 
                     {/* Desktop Navigation */}
